605e3591be6cb82b6db88b0effef91772b903728
marianellamonroyortizhb <1880@holbertonschool.com>
2020-08-25 17:56:27 -0500

    Agregar archivos

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bictia_S2
+Este repositorio contiene los archivos de las prácticas de la segunda semana.
---
+++ script.js
@@ -0,0 +1,19 @@
+function pedirNombre(){
+	var nombre = prompt('¿Cuál es su nombre?')
+	console.log(nombre)
+}
+
+function pedirEdad(){
+	var edad = prompt('¿Cuál es su edad?')
+	console.log(edad)
+}
+
+function pedirCiudad(){
+	var ciudad = prompt('¿Dónde nació?')
+	console.log(ciudad)
+}
+
+function pedirEdad(){
+	var profesion = prompt('¿Cuál es su profesión?')
+	console.log(profesion)
+}
